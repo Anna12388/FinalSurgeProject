8bd36974391553315e976ea8c8ff8aba51d2f3a0
Anna12388 <125131848+Anna12388@users.noreply.github.com>
2023-08-25 16:39:25 +0300

    commit

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,8 +6,11 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pages.*;
 import utils.PropertyReader;
 
@@ -24,21 +27,27 @@ public abstract class BaseTest {
     AccountPage accountPage;
     UserProfilePage userProfilePage;
     WorkoutCalendarPage workoutCalendarPage;
-
+@Parameters({""})
     @Step("Настройка и открытие браузера")
     @BeforeMethod
-    public void setUp() {
-        Configuration.baseUrl = System.getenv().getOrDefault("FINALSURGE_URL", PropertyReader.getProperty("finalsurge.url"));
-        email = System.getenv().getOrDefault("FINALSURGE_EMAIL", PropertyReader.getProperty("finalsurge.email"));
-        password = System.getenv().getOrDefault("FINALSURGE_PASSWORD", PropertyReader.getProperty("finalsurge.password"));
+    public void setUp(@Optional("chrome") String browser) {
 
-
+    if (browser.equals("chrome")) {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    } else{
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+
+    }
+        Configuration.baseUrl = System.getenv().getOrDefault("FINALSURGE_URL", PropertyReader.getProperty("finalsurge.url"));
+        email = System.getenv().getOrDefault("FINALSURGE_EMAIL", PropertyReader.getProperty("finalsurge.email"));
+        password = System.getenv().getOrDefault("FINALSURGE_PASSWORD", PropertyReader.getProperty("finalsurge.password"));
 
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
@@ -47,7 +56,6 @@ public abstract class BaseTest {
         accountPage = new AccountPage(driver);
         userProfilePage = new UserProfilePage(driver);
         workoutCalendarPage = new WorkoutCalendarPage(driver);
-
     }
 
     @Step("Закрытие браузера")
